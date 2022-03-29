9eaf84c6f0e3e7e54e10bd395b8f344fef295dbe
yoda_mac <yoda_mac@sayongjaui-MacBookPro.local>
2022-03-30 08:23:33 +0900

    build시 에러

---
+++ layouts/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+    return <div>test DIV</div>
+}
+
+export default App;
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,8 @@
 import path from 'path';
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+// import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import webpack from 'webpack';
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+// import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -59,12 +59,12 @@ const config: webpack.Configuration = {
     ],
   },
   plugins: [
-    new ForkTsCheckerWebpackPlugin({
-      async: false,
-      // eslint: {
-      //   files: "./src/**/*",
-      // },
-    }),
+    // new ForkTsCheckerWebpackPlugin({
+    //   async: false,
+    //   // eslint: {
+    //   //   files: "./src/**/*",
+    //   // },
+    // }),
     new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
     //TODO: EnvironmentPlugin =>  이걸 넣으면 프론트에서도 process.env.NODE_ENV에 접근 할 수 있다.
   ],
@@ -73,27 +73,27 @@ const config: webpack.Configuration = {
     filename: '[name].js', //TODO: [name].js는 entry로 지정한 파일을 말한다.
     publicPath: '/dist/',
   },
-  devServer: {
-    historyApiFallback: true, // react router
-    port: 3090,
-    publicPath: '/dist/',
-    proxy: {
-      '/api/': {
-        target: 'http://localhost:3095',
-        changeOrigin: true,
-      },
-    },
-  },
+//   devServer: {
+//     historyApiFallback: true, // react router
+//     port: 3090,
+//     publicPath: '/dist/',
+//     proxy: {
+//     '/api/': {
+//         target: 'http://localhost:3095',
+//         changeOrigin: true,
+//     },
+//     },
+// },
 };
 
 if (isDevelopment && config.plugins) { //개발 환경일떄 사용할 플러그인
-  config.plugins.push(new webpack.HotModuleReplacementPlugin());
-  config.plugins.push(new ReactRefreshWebpackPlugin());
-  config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }));
+//   config.plugins.push(new webpack.HotModuleReplacementPlugin());
+//   config.plugins.push(new ReactRefreshWebpackPlugin());
+//   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }));
 }
 if (!isDevelopment && config.plugins) { //개발 환경이 아닐때 사용할 플러그인
-  config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
-  config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
+//   config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+//   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
 }
 
 export default config;
